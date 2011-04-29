c4717c10b66825cad8f724cb69c79f44d76730fc
Adam Coddington <adam@parthenonsoftware.com>
2011-04-29 15:34:36 -0700

    Adding a subclass of CurrentSiteManager accepting a path to a 'site'/'sites' field.

---
+++ managers.py
@@ -0,0 +1,12 @@
+from django.contrib.sites.managers import CurrentSiteManager
+from django.contrib.sites.models import Site
+
+class PathAssistedCurrentSiteManager(CurrentSiteManager):
+    def __init__(self, field_path):
+        super(PathAssistedCurrentSiteManager, self).__init__()
+        self.__field_path = field_path
+
+    def get_query_set(self):
+        return super(CurrentSiteManager, self).get_query_set().filter(
+                    **{self.__field_path: Site.objects.get_current()}
+                )
