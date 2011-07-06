ddbf6ab048e70c0cd844aaf8c589907a9c034af9
Aleksandr Zorin <plazix@gmail.com>
2011-07-06 12:36:24 +0400

    new template loader

--- multisite/template_loader.py
+++ multisite/template_loader.py
@@ -1,28 +1,33 @@
-from django.template import TemplateDoesNotExist
-from django.utils._os import safe_join
-import os.path
-from django.contrib.sites.models import Site
+# -*- coding: utf-8 -*-
+
 from django.conf import settings
+from django.contrib.sites.models import Site
+from django.template.loaders.filesystem import Loader as FilesystemLoader
+from django.utils._os import safe_join
 
-def get_template_sources(template_name, template_dirs=None):
-    template_dir = os.path.join(settings.TEMPLATE_DIRS[0], Site.objects.get_current().domain)
-    try:
-        yield safe_join(template_dir, template_name)
-    except UnicodeDecodeError:
-        raise
-    except ValueError:
-        pass
 
-def load_template_source(template_name, template_dirs=None):
-    tried = []
-    for filepath in get_template_sources(template_name, template_dirs):
-        try:
-            return (open(filepath).read().decode(settings.FILE_CHARSET), filepath)
-        except IOError:
-            tried.append(filepath)
-    if tried:
-        error_msg = "Tried %s" % tried
-    else:
-        error_msg = "Your TEMPLATE_DIRS setting is empty. Change it to point to at least one template directory."
-    raise TemplateDoesNotExist, error_msg
-load_template_source.is_usable = True
+class Loader(FilesystemLoader):
+    def get_template_sources(self, template_name, template_dirs=None):
+        if not template_dirs:
+            template_dirs = settings.TEMPLATE_DIRS
+
+        domain = Site.objects.get_current().domain
+        default_dir = getattr(settings, 'MULTISITE_DEFAULT_DIR', 'default')
+
+        new_template_dirs = []
+        for template_dir in template_dirs:
+            new_template_dirs.append(safe_join(template_dir, domain))
+            if default_dir:
+                new_template_dirs.append(safe_join(template_dir, default_dir))
+
+        for template_dir in new_template_dirs:
+            try:
+                yield safe_join(template_dir, template_name)
+            except UnicodeDecodeError:
+                # The template dir name was a bytestring that wasn't valid UTF-8.
+                raise
+            except ValueError:
+                # The joined path was located outside of this particular
+                # template_dir (it might be inside another one, so this isn't
+                # fatal).
+                pass
