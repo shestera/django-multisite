2a257345fb6e76c36214f5f5a9fe5510b50b0ebd
Simon Law <simon.law@ecometrica.com>
2012-06-04 17:10:42 -0400

    setup.py: Add long_description for PyPi.

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from distutils.core import setup
+import os
+
+_dir_ = os.path.dirname(__file__)
+
+
+def long_description():
+    """Returns the value of README.rst"""
+    with open(os.path.join(_dir_, 'README.rst')) as f:
+        return f.read()
 
 
 setup(name='django-multisite',
       version='0.1',
       description='Multisite for Django',
+      long_description=long_description(),
       author='Leonid S Shestera',
       author_email='leonid@shestera.ru',
       url='http://github.com/shestera/django-multisite',
