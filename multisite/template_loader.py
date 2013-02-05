082a0bf265f2c2c8150e5f79171ba0f7f934c3a2
Daniel Gray <dan@technigami.com>
2013-02-05 10:34:50 -0800

    updating multisite to work in app directories.

--- multisite/template_loader.py
+++ multisite/template_loader.py
@@ -3,15 +3,24 @@ from django.utils._os import safe_join
 import os.path
 from django.contrib.sites.models import Site
 from django.conf import settings
+from django.template.loaders.app_directories import *
+
 
 def get_template_sources(template_name, template_dirs=None):
     template_dir = os.path.join(settings.TEMPLATE_DIRS[0], Site.objects.get_current().domain)
-    try:
-        yield safe_join(template_dir, template_name)
-    except UnicodeDecodeError:
-        raise
-    except ValueError:
-        pass
+    if not template_dirs:
+        template_dirs = app_template_dirs
+        for template_dir in template_dirs:
+            try:
+                template_dir = os.path.join(template_dir, Site.objects.get_current().domain)
+                yield safe_join(template_dir, template_name)
+            except UnicodeDecodeError:
+                # The template dir name was a bytestring that wasn't valid UTF-8.
+                raise
+            except ValueError:
+                # The joined path was located outside of template_dir.
+                pass
+                
 
 def load_template_source(template_name, template_dirs=None):
     tried = []
