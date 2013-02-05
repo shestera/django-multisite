540b7d75016a4624e5f6be9584c44c92426135fc
Daniel Gray <dan@technigami.com>
2013-02-05 11:18:01 -0800

    updating the multi-site loader to support localshot:8000 / sites that have a port number

--- multisite/template_loader.py
+++ multisite/template_loader.py
@@ -8,17 +8,32 @@ from django.template.loaders.app_directories import *
 
 def get_template_sources(template_name, template_dirs=None):
     template_dir = os.path.join(settings.TEMPLATE_DIRS[0], Site.objects.get_current().domain)
+
     if not template_dirs:
+        
         template_dirs = app_template_dirs
         for template_dir in template_dirs:
             try:
-                template_dir = os.path.join(template_dir, Site.objects.get_current().domain)
+                host = Site.objects.get_current().domain
+                
+                # template directories shouldn't need to include port number 
+                # so remove the port from the sites domain name for the search path.
+                try:
+                    host = host.rsplit(":")[0]
+                except:
+                    pass
+                
+                    
+                template_dir = os.path.join(template_dir, host)
                 yield safe_join(template_dir, template_name)
+                
             except UnicodeDecodeError:
                 # The template dir name was a bytestring that wasn't valid UTF-8.
                 raise
+                
             except ValueError:
                 # The joined path was located outside of template_dir.
+                
                 pass
                 
 
