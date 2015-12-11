0acb281cbd335d7c1d3b857238eec08123d53e37
Matthieu Hughes <matthieuhughes.1@gmail.com>
2015-12-11 16:15:52 -0400

    Change return type of get_template_sources() in filesystem loader
    Django has changed some template loading code in 1.9, and
    it's now expected that a django.template.Origin object
    is returned from get_template_sources()

--- multisite/template/loaders/filesystem.py
+++ multisite/template/loaders/filesystem.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 from django.core.exceptions import SuspiciousFileOperation
 from django.conf import settings
 from django.contrib.sites.models import Site
+try:
+    from django.template import Origin
+except ImportError:
+    # Django < 1.9
+    pass
 from django.template.loaders.filesystem import Loader as FilesystemLoader
 from django.utils._os import safe_join
 
@@ -24,7 +29,7 @@ class Loader(FilesystemLoader):
 
         for template_dir in new_template_dirs:
             try:
-                yield safe_join(template_dir, template_name)
+                name = safe_join(template_dir, template_name)
             except UnicodeDecodeError:
                 # The template dir name was a bytestring that wasn't valid UTF-8.
                 raise
@@ -33,3 +38,14 @@ class Loader(FilesystemLoader):
                 # template_dir (it might be inside another one, so this isn't
                 # fatal).
                 pass
+
+            # Template loading was changed in Django 1.9, and
+            # django now expects an Origin object to be returned
+            try:
+                yield Origin(
+                    name=name,
+                    template_name=template_name,
+                    loader=self
+                )
+            except NameError:
+                yield name
